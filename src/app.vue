2193d3eebfd1f0ad468b348e87669c31c122e5f5
wangchao <wangchao@daojia.com>
2019-04-24 16:46:48 +0800

    complete devserver

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+$black: black;
+$yellow: yellow;
+h2 {
+  font-size: 40px;
+  color: $black;
+  background: $yellow;
+}
+</style>
+
